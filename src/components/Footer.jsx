3076d1d817f2ecdd26fc64437c6de4414bb3d014
Aditya <37655818+adi-075@users.noreply.github.com>
2023-03-02 12:38:57 +0530

    Add Home and style Navbar

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,25 +1,61 @@
-import React from 'react';
-import './FooterStyles.css';
-import { FaFacebook, FaLinkedin, FaInstagram, FaYoutube } from 'react-icons/fa';
+import React from "react";
+import "./FooterStyles.css";
+import { FaFacebook, FaLinkedin, FaInstagram, FaYoutube } from "react-icons/fa";
 
 const Footer = () => {
-    return (
-      <>
-        <div className="footer">
-                <div className="content">
-                    <div className="rights">
-                        <p>Copyright &copy; 2023. All rights reserved.</p>
-                    </div>  
-                    <div className="footer-bottom">
-                        <FaFacebook size={20} style={{ color: '#d3d3d3', marginRight: '10px' }} />
-                        <FaInstagram size={20} style={{ color: '#d3d3d3', marginRight: '10px' }} />
-                        <FaLinkedin size={20} style={{ color: '#d3d3d3', marginRight: '10px' }} />
-                        <FaYoutube size={20} style={{color: '#d3d3d3', marginRight: '10px'}} />
-                    </div>
-                </div>
-            </div>
-            </>
-  )
-}
+  return (
+    <>
+      <div className="footer">
+        <div className="content">
+          <div className="rights">
+            <p>Copyright &copy; 2023. All rights reserved.</p>
+          </div>
+          <div className="footer-bottom">
+            <a
+              href="https://www.facebook.com/vidyalankar.VIIE"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaFacebook
+                size={20}
+                style={{ color: "#d3d3d3", marginRight: "10px" }}
+              ></FaFacebook>
+            </a>
+            <a
+              href="https://www.youtube.com/@viievidyalankar3834"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaYoutube
+                size={20}
+                style={{ color: "#d3d3d3", marginRight: "10px" }}
+              />
+            </a>
+            <a
+              href="https://www.instagram.com/viie_college/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaInstagram
+                size={20}
+                style={{ color: "#d3d3d3", marginRight: "10px" }}
+              />
+            </a>
+            <a
+              href="https://in.linkedin.com/company/vidyalankar-institute-for-international-education"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaLinkedin
+                size={20}
+                style={{ color: "#d3d3d3", marginRight: "10px" }}
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,52 +1,36 @@
 import React from "react";
-import Navbar from "./Navbar";
 import "../index.css";
-// import Footer from "./Footer";
-// import Signup from "./Signup";
 
 const Home = () => {
   return (
     <>
-      <Navbar />
-      <section class="hero" className="w-auto min-h-full min-w-full"></section>
-
-      <div className="flex bg-gray-100">
-        <div className="flex-1">
-          <h1 className="w-full text-center py-12 px-4 text-5xl font-semibold text-red-900">
-            Discover VIIE
-          </h1>
-          <p className="text-xl text-gray-900 px-12 ">
-            Vidyalankar has collaborated with top universities in the USA,
-            CANADA, UK, Australia, and New Zealand and has developed some unique
-            programs at the undergraduate and postgraduate levels. Our programs
-            equip the students with the right toolset, suitable skillset and,
-            more importantly, the right mindset. Ever since they were started in
-            2006, they have been well received by the students, their parents
-            and by the industry.
-          </p>
-          <p className="text-xl text-gray-900 px-12 ">
-            Our Global Pathway program gives students the best of both worlds;
-            studying initial years with us in India offers students a solid
-            foundation. The academic rigor makes them ready to take on
-            international challenges eventually. Vidyalankar has a legacy of
-            countless successful candidates over these 15 years of operation.
-            {/* <b className="flex flex-col text-center py-5">We are VIIE</b> */}
-          </p>
-        </div>
-        <div className="flex">
-          <iframe
-            className="px-11 py-6"
-            width="854"
-            height="480"
-            src="https://www.youtube.com/embed/tBDfWEc2CLA"
-            title="Vidyalankar Institute For International Education"
-            frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowfullscreen
-          ></iframe>
+      <div class="w-full h-full">
+        <div className="min-h-screen">
+          <div className="w-full mx-auto text-center text-2xl sm:text-4xl py-5">
+            <h1>This is VIIE</h1>
+          </div>
+          <div className="max-w-screen mx-auto flex flex-col sm:flex-row">
+            <div className="flex text-gray-500 text-center px-10 text-2xl sm:px-12">
+              Vidyalankar has collaborated with top universities in the USA,
+              CANADA, UK, Australia, and New Zealand and has developed some
+              unique programs at the undergraduate and postgraduate levels.Our
+              programs equip the students with the right toolset, suitable
+              skillset and, more importantly, the right mindset. Ever since they
+              were started in 2006, they have been well received by the
+              students, their parents and by the industry.
+            </div>
+            <div className="flex order-1 sm:order-0 mx-auto py-6 sm:mx-7 sm:flex">
+              <iframe
+                src="https://www.youtube.com/embed/tBDfWEc2CLA"
+                title="Vidyalankar Institute For International Educaition"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                allowfullscreen
+              ></iframe>
+            </div>
+          </div>
         </div>
       </div>
-      {/* <Footer /> */}
     </>
   );
 };
